2f5701469c7fe4365acf8d0c1a2aa89890bcc841
laterDays <dadamszx@gmail.com>
2014-12-19 00:43:34 -0500

    polished webpage

--- webcrawler.php
+++ webcrawler.php
@@ -17,13 +17,17 @@ header("Cache-Control: no-cache, must-revalidate");
 	{
 		$OPT = 2;
 		$query = $_POST['query'];
-		$cmd = "./crawl -c \"$query\" \"graph\" > out".$INSTANCE.".txt 2>&1 &";
+		$cmd = "./crawl -q \"$query\" \"graph\" > out".$INSTANCE.".txt 2>&1 &";
 		exec($cmd);
 	}
-	else if(!empty($_POST['delete_request']))
+	else if(!empty($_POST['s_values']))
 	{
-		$OPT = 3;
-		$cmd = "./crawl -c \"MATCH ()-[r]->(), (n) DELETE r,n\" \"graph\" > out".$INSTANCE.".txt 2>&1 &";
+		$OPT = 4;
+		$EDGENODE = $_POST['s_values'][0];
+		$IDLABELPROPERTIES = $_POST['s_values'][1];
+		$PROPERTY = $_POST['s_values'][2];
+		$VALUE = $_POST['s_values'][3];
+		$cmd = "./crawl -pq $EDGENODE $IDLABELPROPERTIES '$PROPERTY' '$VALUE' > out".$INSTANCE.".txt 2>&1 &";
 		exec($cmd);
 	}
 ?>
@@ -32,14 +36,14 @@ header("Cache-Control: no-cache, must-revalidate");
 <style type="text/css">
 	#container {
 		position: absolute;
-		left: 300px;
+		left: 330px;
 		top: 0px;
 		background-color: #ffffff;
 		min-width: 700px;
 		height: 100%;
 		margin: auto;
 	}
-	BODY, TD, P {
+	BODY, TD, P, DIV {
 		font-family: arial,helvetica,sans-serif;
 		font-size: 10px;
 		color: #333333;
@@ -50,32 +54,56 @@ header("Cache-Control: no-cache, must-revalidate");
 <table width="300px" id="side_panel">
 	<tr>
 		<td width="100%">
-			<form name="crawl_form" method="post" action="webcrawler.php">
-				<input size="17" type="text" value="" name="crawl_url[]"/>
-				<input size="4" type="text" value="" name="crawl_url[]"/>
-				<input size="10" type="Submit" value="crawl" name="crawl_submit"/>
-			</form>
-			<form name="query_form" method="post" action="webcrawler.php">
-				<input size="20" type="text" value="" name="query"/>
-				<input size="10" type="Submit" value="run query" name="query_submit"/>
-			</form>
-			<select>
-				<option value="1">1</option>
-			</select>
-			<?php echo "[".$OPT."] ".$cmd; ?>
-			<div id="content" style="border: 1px solid white; background-color: #ffffff; width: 280px; overflow: scroll;">
+			<div id="raw_queries" style="padding: 2px; box-shadow: 0px 10px 5px #888888;">
+				<form name="crawl_form" method="post" action="webcrawler.php">
+					<input size="17" type="text" value="" name="crawl_url[]"/>
+					<input size="4" type="text" value="" name="crawl_url[]"/>
+					<input size="10" type="Submit" value="crawl" name="crawl_submit"/>
+				</form>
+				<form name="query_form" method="post" action="webcrawler.php">
+					<input size="20" type="text" value="" name="query"/>
+					<input size="10" type="Submit" value="run query" name="query_submit"/>
+				</form>
+			</div>
+			<br>
+			<div id="pieced_search" style="padding: 2px;box-shadow: 0px 10px 5px #888888;">
+				<form name="s_values" method="post" action="webcrawler.php">
+					<select name="s_values[]">
+						<option value="nodes">nodes</option>
+						<option value="edges">edges</option>
+					</select>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					property &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					value
+					<br>
+					<select name="s_values[]">
+						<option value="''"></option>
+						<option value="id">id</option>
+						<option value="label">label</option>
+						<option value="properties">properties</option>
+					</select>
+					<input size="10" type="text" value="" name="s_values[]"/>
+					<input size="10" type="text" value="" name="s_values[]"/>
+					<br>
+					<input size="10" type="Submit" value="run query" name="s_values_submit"/>
+				</form>
+			</div>
+			<br>
+			<div id="content" style="padding: 2px;border: 1px solid white; background-color: #ffffff; width: 300px; overflow: scroll; box-shadow: 0px 10px 5px #888888;">
 				<?php 
 					include_once('console.php'); 
 				?>
 			</div>
+			
+
 		</td>	
 	</tr>
 	<tr>
 		<td>
-			<br>
-			<form name="delete" method="post" action="webcrawler.php">
-				<button name="delete_request" style="background-color: #ff6666"><font color="white"> DELETE </font></button>
-			</form>
+ 			<button onclick="loadGraph()">REFRESH GRAPH</button> 
+			<div id="g_info" style="padding: 2px;border: 1px solid white; background-color: #ffffff; width: 300px; height: 100px; overflow: scroll; box-shadow: 0px 10px 5px #888888;"">
+
+			</div>
 		</td>
 	</tr>
 </table>
@@ -91,35 +119,45 @@ header("Cache-Control: no-cache, must-revalidate");
 		This section is for drawing the graph. It uses the Sigma.js 
 		library.
 	*/
-  	sigma.parsers.json(
-		'out_graph.json', 
-		{
-			container: 'container',
-			settings: 
+  	function loadGraph () 
+	{
+		$('#container').empty();
+		sigma.parsers.json(
+			'out_graph.json', 
 			{
-				defaultNodeColor: '#3febeb',
-				defaultLabelColor: '#333333'
-			}
-		},
-		function(s)
-		{
-			var i,
-			nodes = s.graph.nodes(),
-			len = nodes.length;
+				container: 'container',
+				settings: 
+				{
+					defaultNodeColor: '#3febeb',
+					defaultLabelColor: '#333333'
+				}
+			},
+			function(s)
+			{
+				var i,
+				nodes = s.graph.nodes(),
+				len = nodes.length;
 
-			for (i = 0; i < len; i++)
-			{
-				nodes[i].x = Math.random();
-				nodes[i].y = Math.random();
-				nodes[i].size = s.graph.degree(nodes[i].id);
-				//nodes[i].color = nodes[i].id;//nodes[i].center ? '#333' : '#666#'
-			}
+				for (i = 0; i < len; i++)
+				{
+					nodes[i].x = Math.random();
+					nodes[i].y = Math.random();
+					nodes[i].size = 1 + s.graph.degree(nodes[i].id);
+					//nodes[i].color = nodes[i].id;//nodes[i].center ? '#333' : '#666#'
+				}
 			
-			s.refresh();
-			s.startForceAtlas2();
-		}
-	);
-	
+				s.refresh();
+				s.startForceAtlas2();
+
+				s.bind('clickNode', function(e) {
+					$('#g_info').html(
+						"id: " + e.data.node.id + "<br>" +
+						"labels:" + e.data.node.label);
+				});
+			}
+		);
+	}
+	loadGraph();
 
 	/*
 		Set the size of the console. It doesn't like to behave
@@ -129,7 +167,7 @@ header("Cache-Control: no-cache, must-revalidate");
 	var graph_height = graph_canvas.offsetHeight;	// get actual height using .offsetHeight
 							// .height is undefined
 	var contentDiv = document.getElementById('content');
-	contentDiv.style.height = graph_height - 200;
+	contentDiv.style.height = graph_height - 335;
 	//contentDiv.scrollTop = contentDiv.scrollHeight;
 
 	
